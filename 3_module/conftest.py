ddb1987d20a667a6afd6c8bfe2f8ab5d064c6d95
Ayrat Khamatshin PC <airatko4@gmail.com>
2023-10-31 15:50:01 +0500

    Итоговое задание

--- 3_module/conftest.py
+++ 3_module/conftest.py
@@ -2,16 +2,27 @@ from selenium.webdriver.chrome.options import Options
 import pytest
 from selenium import webdriver
 
-
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default="en",
                      help="Choose the browser language")
+    parser.addoption('--browser_name', action='store', default="chrome",
+                     help="Choose browser: chrome or firefox")
 
 @pytest.fixture(scope="function")
 def browser(request):
     user_language = request.config.getoption("language")
-    options = Options()
-    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-    browser = webdriver.Chrome(options=options)
+    browser_name = request.config.getoption("browser_name")
+
+    if browser_name == "chrome":
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        browser = webdriver.Chrome(options=options)
+    elif browser_name == "firefox":
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        browser = webdriver.Firefox(options=options)
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
+
     yield browser
     browser.quit()
